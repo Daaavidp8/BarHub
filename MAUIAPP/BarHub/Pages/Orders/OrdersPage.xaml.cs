a70d46df2338152b51c777630e7887f0571f3a6e
David Picazo Rocafull <106530170+Daaavidp8@users.noreply.github.com>
2025-05-29 21:02:12 +0200

    arreglado release

--- MAUIAPP/BarHub/Pages/Orders/OrdersPage.xaml.cs
+++ MAUIAPP/BarHub/Pages/Orders/OrdersPage.xaml.cs
@@ -10,6 +10,7 @@ public partial class OrdersPage : BarHubBaseContentPage
 	public OrdersPage(User user, IServiceProvider services)
 	{
 		InitializeComponent();
+        Console.WriteLine("Aqui llegaaaaaa, OrdersPage initialized");
         var gets = services.GetService<Gets>();
         var deletes = services.GetService<Deletes>();
         var puts = services.GetService<Puts>();
@@ -19,5 +20,7 @@ public partial class OrdersPage : BarHubBaseContentPage
         var waiterViewModel = new OrderPageViewModel(orderLineContext,gets, puts, user);
 
         BindingContext = waiterViewModel;
+
+        Console.WriteLine("Aqui nooooooo llegaaaaaa, OrdersPage initialized BLABLALALBLALBLAL");
     }
 }
